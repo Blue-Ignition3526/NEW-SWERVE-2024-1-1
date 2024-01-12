f4180acb4fad4b6ae12bea176a2d512c69ace64c
Adrian Rangel Martinez <142278016+AdrianRang@users.noreply.github.com>
2024-01-12 16:17:09 -0600

    Separated Active track to its own command

--- src/main/java/frc/robot/commands/ActiveTrack.java
+++ src/main/java/frc/robot/commands/ActiveTrack.java
@@ -4,6 +4,7 @@
 
 package frc.robot.commands;
 
+import java.util.Optional;
 import java.util.function.Supplier;
 
 import edu.wpi.first.math.filter.SlewRateLimiter;
@@ -11,8 +12,9 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.Constants.Swerve.Physical;
 import frc.robot.subsystems.SwerveDrive;
+import frc.robot.subsystems.VisionSubsystem;
 
-public class DriveSwerve extends Command {
+public class ActiveTrack extends Command {
   /**
    * Swerve drive subsystem
    */
@@ -54,19 +56,26 @@ public class DriveSwerve extends Command {
   SlewRateLimiter rotLimiter = new SlewRateLimiter(Constants.Swerve.Physical.kTeleopMaxAngularAccelerationUnitsPerSecond);
 
   /**
+   * Vision subsystem
+   */
+  VisionSubsystem vision;
+
+  /**
    * Creates a new DriveSwerve command.
    * @param m_swerveDrive The swerve drive subsystem
+   * @param vision The vision subsystem
    * @param x The x speed supplier
    * @param y The y speed supplier
-   * @param rot The rotation speed supplier
+   * @param rot The rotaion speed if there is no AprilTag found
    * @param fieldRelative Whether the drive is field relative
    */
-  public DriveSwerve(SwerveDrive m_swerveDrive, Supplier<Double> x, Supplier<Double> y, Supplier<Double> rot, Supplier<Boolean> fieldRelative) {
+  public ActiveTrack(SwerveDrive m_swerveDrive, VisionSubsystem vision, Supplier<Double> x, Supplier<Double> y, Supplier<Double> rot, Supplier<Boolean> fieldRelative) {
     this.m_swerveDrive = m_swerveDrive;
     this.xSpeed = x;
     this.ySpeed = y;
     this.rotSpeed = rot;
     this.fieldRelative = fieldRelative;
+    this.vision = vision;
     addRequirements(m_swerveDrive);
   }
 
@@ -80,13 +89,23 @@ public class DriveSwerve extends Command {
     // Get the current speeds
     double xSpeed = this.xSpeed.get();
     double ySpeed = this.ySpeed.get();
-    double rotSpeed = this.rotSpeed.get();
+    double rotSpeed;
 
+    boolean tagIsVisible;
+    
+    Optional<Double> rotSpeedOpt = vision.calculateRotSpeed(Constants.Vision.AprilTags.kSpeakerTagID);
+    if (rotSpeedOpt.isPresent()){
+      rotSpeed = rotSpeedOpt.get();
+      tagIsVisible = true;
+    }else{
+      rotSpeed = this.rotSpeed.get();
+      tagIsVisible = false;
+    }
 
     // If the speeds are lower than the deadzone
     xSpeed = Math.abs(xSpeed) > Constants.Operator.kDeadzone ? xSpeed : 0.0;
     ySpeed = Math.abs(ySpeed) > Constants.Operator.kDeadzone ? ySpeed : 0.0;
-    rotSpeed = Math.abs(rotSpeed) > Constants.Operator.kDeadzone ? rotSpeed : 0.0;
+    if (!tagIsVisible) rotSpeed = Math.abs(rotSpeed) > Constants.Operator.kDeadzone ? rotSpeed : 0.0;
 
     // Multiply by the top speed
     xSpeed = xLimiter.calculate(xSpeed) * Physical.kTeleopMaxSpeedMetersPerSecond / 2;
